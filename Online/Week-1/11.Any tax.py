c7a96251fc76bb7b194d401351da633160a9c745
Max180643 <39792693+max180643@users.noreply.github.com>
2018-05-31 22:25:17 +0700

    Delete 11.Any tax.py

--- Online/Week-1/11.Any tax.py
+++
@@ -1,9 +0,0 @@
-"""Any Tax"""
-def main():
-    """Main Function"""
-    price = int(input())
-    price_service = price + (price * 0.10)
-    price_vat = price_service + (price_service * 0.07)
-    print(int(price_vat))
-
-main()
