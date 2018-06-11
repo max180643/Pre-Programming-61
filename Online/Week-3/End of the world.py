303aa7071246afac8383ea5d01435cfe07799979
Max180643 <39792693+max180643@users.noreply.github.com>
2018-06-11 23:24:18 +0700

    Add files via upload

--- Online/Week-3/End of the world.py
+++ Online/Week-3/End of the world.py
@@ -1,7 +1,22 @@
 """"End of the world"""
 def main():
     """Main Function"""
+    # input
     msg = input()
     power = int(input())
     defense = int(input())
-    if msg == "Justify yourself!"
+    # condition
+    if defense > power * 30 / 100:
+        power = power - (power * 45 / 100)
+    if msg == "Justify yourself!":
+        power = power - (power * 20 / 100)
+    # check
+    total = power - defense
+    if total <= 0:
+        print("We saved the world!")
+    elif total > 1000:
+        print("End of the world.")
+    else:
+        print("A great loss.")
+
+main()
