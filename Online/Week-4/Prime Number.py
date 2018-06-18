e4ce0d792b38498a1c5639f8df8a609ec525934d
Max180643 <39792693+max180643@users.noreply.github.com>
2018-06-18 10:49:47 +0700

    Add files via upload

---
+++ Online/Week-4/Prime Number.py
@@ -0,0 +1,15 @@
+"""Prime Number"""
+def main():
+    """Main Function"""
+    number = int(input())
+    if number > 1:
+        for i in range(2, number):
+            if (number % i) == 0:
+                print("No")
+                break
+        else:
+            print("Yes")
+    else:
+        print("No")
+
+main()
