bbcd019bc24df24fe03b613afc1afcbfca3732d4
Max180643 <39792693+max180643@users.noreply.github.com>
2018-07-06 00:15:25 +0700

    Update Pen Pineapple Apple Pen.py

--- Onsite/Week-2/Thursday/Pen Pineapple Apple Pen.py
+++ Onsite/Week-2/Thursday/Pen Pineapple Apple Pen.py
@@ -1,17 +1,33 @@
 """Pen Pineapple Apple Pen"""
 def main():
     """Main Function"""
-    while True:
-        text = input()
-        if text == "Pen":
-            text = input()
-            if text == "Pineapple":
-                text = input()
-                if text == "Apple":
-                    text = input()
-                    if text == "Pen":
-                        print("Correct lyrics!")
-                        break
-        print("Wrong lyrics!")
+    check = 0
+    temp = ""
+    var = True
+    num = 0
+    while var:
+        text = str(input())
+        num += 1
+        if text == "Pen" and check == 0:
+            check += 1
+        if text == "Pineapple" and check == 1:
+            check += 1
+        if text == "Apple" and check == 2:
+            check += 1
+        if text == "Pen" and check == 3:
+            check += 1
+
+        if check != num and text != "Pen":
+            temp = temp + "Wrong lyrics!\n"
+            print(check,num,text)
+            check = 0
+            num = 0
+
+
+        if check == 4:
+            temp = temp + "Correct lyrics!"
+            var = False
+        print(check, temp)
+    print(temp)
 
 main()
